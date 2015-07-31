af396ae6d1ae41c6739439ebecf86298abb10d64
Micke <micke@home.com>
2015-08-01 01:06:25 +0200

    Created Project

---
+++ Bastuenhet6/Bastuenhet6.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Bastuenhet6.ino
+ Created:	8/1/2015 1:05:01 AM
+ Author:	Micke
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
